514d0569c7530dca4dcce0d2e5b451c461c9e706
adeclerk <adeclerk@gmail.com>
2013-04-19 13:22:34 -0300

    Update LlticDbConnection.inc.php

--- backend/LlticDbConnection.inc.php
+++ backend/LlticDbConnection.inc.php
@@ -41,38 +41,6 @@ class LlticDbConnection
   }
 }
 
-class SessionRecord
-{
-	private $sessionID;
-	private $sessionUsername;
-	private $sessionStart;
-	private $isSet;
-	
-	public function __construct()
-	{
-		$this->isSet = False;
-	}
-	
-	public function __destruct()
-	{
-		
-	}
-	
-	public function set(array $row)
-	{
-		$this->sessionID 	= $row[0];
-		$this->sessionUsername 	= $row[1];
-		$this->sessionStart 	= $row[2];
-	}
-	
-	public function isExpired()
-	{
-		if(($this->sessionStart - 84600) < 0)
-			return True;
-		else
-			return False;
-	}
-}
 class EmployeeTable
 {
   private $dbConnection;
