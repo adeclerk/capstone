0d92912b4e12a3d5b048f884cef40d9ec823e110
adeclerk <adeclerk@gmail.com>
2013-04-19 14:13:12 -0400

    Updated Session class

--- backend/Session.inc.php
+++ backend/Session.inc.php
@@ -1,55 +1,98 @@
 <?php
 require once 'LlticDbConnection.inc.php';
-class SessionRecord
-{
-  private $sessionID;
-	private $sessionUsername;
-	private $sessionStart;
-	private $isSet;
-	
-	public function __construct()
-	{
-		$this->isSet = False;
-	}
-	
-	public function __destruct()
-	{
-		
-	}
-	
-	public function set(array $row)
-	{
-		$this->sessionID 	= $row[0];
-		$this->sessionUsername 	= $row[1];
-		$this->sessionStart 	= $row[2];
-	}
-	
-	public function isExpired()
-	{
-		if(($this->sessionStart - 84600) < 0)
-			return True;
-		else
-			return False;
-	}
-}
 
 class Session
 {
-
-  private $isAlive = false;
- function __construct()
-  { 
-    
+  private $dbcon;
+  private $isAlive = true;
+  
+  public function __construct()
+  {
+    session_set_save_handler(
+      array(&$this, 'open'),
+      array(&$this, 'close'),
+      array(&$this, 'read'),
+      array(&$this, 'write'),
+      array(&$this, 'destroy'),
+      array(&$this, 'clean'));
+ 
+    session_start();
   }
 
-  function __destruct()
+  public function __destruct()
   {
-
+    if($this->isAlive)
+    {
+      session_write_close();
+      $this->isAlive = false;
+    }
   }
   
-  function start($user)
+  public function delete()
   {
-  	
+    if(ini_get('session.use_cookies'))
+    {
+      $params = session_get_cookie_params();
+      setcookie(session_name(), '', time() - 42000,
+        $params['path'], $params['domain'],
+        $params['secure'], $params['httponly']
+      );
+    }
+ 
+    session_destroy();
+ 
+    $this->alive = false;
+  }
+ 
+  private function open()
+  {
+    $this->dbcon = new LlticDbConnection;
+  }
+
+  private function close()
+  {
+    $this->dbcon->__destruct();
+    $this->isAlive = false;
+  }
+
+  private function read($sessionID)
+  {
+    $sqlQuery = "SELECT `data` FROM `sessions` WHERE id=`" .$this->dbcon->getConnection()->real_escape_string($sessionID) ."' LIMIT 1";
+
+    $result = $this->dbcon->query($sqlQuery);
+    if($result->num_rows == 1)
+      {
+	$fields = $result->fetch_assoc();
+	return $fields['data'];
+      }
+    else
+      {
+	return '';
+      }
+  }
+
+  private function write($sessionID, $data)
+  {
+    $sqlQuery = "REPLACE INTO `sessions` (`id`,`data`) VALUES('" . $this->dbcon->getConnection()->real_escape_string($sessionID) . "," . $this->dbcon->getConnection()->real_escape_string($data) . "')";
+    $this->dbcon->getConnection()->query($sqlQuery);
+
+    return $this->dbcon->getConnection()->affected_rows;
+  }
+
+  private function destroy($sessionID)
+  {
+    $sqlQuery = "DELETE FROM `sessions` WHERE `id`= '" . $this->dbcon->getConnection->real_escape_string($sessionID) . "'";
+    $this->dbcon->getConnection()->query($sqlQuery);
+
+    $_SESSION = array();
+    return $this->dbcon->getConnection()->affected_rows;
+  }
+
+  private function clean()
+  {
+    $sqlQuery = "DELETE FROM `sessions` WHERE DATE_ADD(`last_accessed`, INTERVAL ".(int) $expire." SECOND) < NOW()"; 
+    $this->dbcon->getConnection()->query($sqlQuery);
+    return $this->dbcon->getConnection()->affected_rows;
   }
 }
 
