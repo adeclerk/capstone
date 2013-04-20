c36655f41166172de6e8a249d571535067478f45
adeclerk <adeclerk@gmail.com>
2013-04-20 15:48:43 -0400

    updated

--- llticbackend/showUsers.php
+++ llticbackend/showUsers.php
@@ -4,7 +4,7 @@ require ('LlticDbConnection.inc.php');
 
 $database = new LlticDbConnection();
 	$result = $database->users->getAllUsers();
-	$row = $result->fetch_assoc();
-	print $row['id'] . " <br/> " . $row['username'];
+
+	print $result->id . " : " .$result->username;
 
 ?>
