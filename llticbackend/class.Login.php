bf73efc7ae5c80c0dbed1f794edf7b9b70a8aec8
adeclerk <adeclerk@gmail.com>
2013-04-20 18:43:56 -0400

    removed code

--- llticbackend/class.Login.php
+++ llticbackend/class.Login.php
@@ -22,22 +22,7 @@ class Login
   
   public function login($username, $password)
   {
-  	$userFound = $this->database->users->findUser($username);
-  	if(!$userFound)
-  	{
-  		print "Invalid Username.";
-  		exit();
-  	}
 
-  	if($userFound->getPassword() == hashPassword($password))
-  	{
-  		//$_SESSION['username'] = $record['username'];
-  		print "SUCCESS";
-  	}
-  	else
-  	{
-  		print "Invalid Password.";
-  	}
   }
   
   public function logout()
