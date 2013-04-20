e753f4bb0445195e8a9d1290c8bca32dfdc30852
adeclerk <adeclerk@gmail.com>
2013-04-20 19:28:15 -0400

    test

--- llticbackend/loginHandler.php
+++ llticbackend/loginHandler.php
@@ -6,9 +6,8 @@ require('class.Login.php');
 error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
   {
-  	print "IN";
 	$logmein = LoginUser();
-	if($logmein->loginUser($_POST['username'],$_POST['password']) == NULL)
+	if(!$logmein->loginUser($_POST['username'],$_POST['password']))
 			{
 			print 'Error invalid username or password';
 			}
