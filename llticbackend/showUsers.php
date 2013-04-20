8e65ee3ee00ef6c0ad29a09ba5d86b839d1b6ad6
adeclerk <adeclerk@gmail.com>
2013-04-20 16:18:15 -0400

    test unit

--- llticbackend/showUsers.php
+++ llticbackend/showUsers.php
@@ -3,10 +3,10 @@ require ('LlticDbConnection.inc.php');
 
 
 $database = new LlticDbConnection();
-	$result = $database->users->getAllUsers();
+//	$result = $database->users->getAllUsers();
 
 	//$index = 0;
-//	$user = $database->users->getUser();
+    $user = $database->users->getUser();
 	
 	//	print $user->getId() . " : " . $user->getUsername() . " : " . $user->getPassword() . " : " . $user->getAdmin() . " <br />";
 	
