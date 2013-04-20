341e9f627338259bdc813a9bc57ac7d49bd63015
adeclerk <adeclerk@gmail.com>
2013-04-20 15:45:40 -0400

    updated syntax

--- llticbackend/showUsers.php
+++ llticbackend/showUsers.php
@@ -3,7 +3,7 @@ require_once('LlticDbConnection.inc.php');
 require_once('User.inc.php');
 
 $database = new LlticDbConnection();
-	$result = $database->users->
+	$result = $database->users->getAllUsers();
 	$row = $result->fetch_assoc();
 	print $row['id'] . " <br/> " . $row['username'];
 
