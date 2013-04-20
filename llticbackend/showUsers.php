0fffa83329774ddaa4909eaf6343974a5b660ec6
adeclerk <adeclerk@gmail.com>
2013-04-20 16:56:17 -0400

    update

--- llticbackend/showUsers.php
+++ llticbackend/showUsers.php
@@ -8,7 +8,7 @@ $database = new LlticDbConnection();
 	//$index = 0;
  // $user = $database->users->getUser();
   	$result = $database->users->getAllUsers();
-  	print '' . $result[0]->getId();
+  	//print '' . $result[0]->getId();
   //	$user = $result;
   	//print $user->getId() . " : " . $user->getUsername() . " : " . $user->getPassword() . " : " . $user->getAdmin() . " <br />";
   
