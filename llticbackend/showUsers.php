4eda6b7d96c7f45b21150e3d340e621c32c94a2d
adeclerk <adeclerk@gmail.com>
2013-04-20 16:16:21 -0400

    test unit

--- llticbackend/showUsers.php
+++ llticbackend/showUsers.php
@@ -6,10 +6,9 @@ $database = new LlticDbConnection();
 	//$result = $database->users->getAllUsers();
 
 	//$index = 0;
-	while($user = $database->users->getUser())
-	{
+	$user = $database->users->getUser();
+	
 		print $user->getId() . " : " . $user->getUsername() . " : " . $user->getPassword() . " : " . $user->getAdmin() . " <br />";
-		
-	}
+	
 
 ?>
