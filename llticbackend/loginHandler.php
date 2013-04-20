70b8c57d1d8aa59d43a85c81f400f73dd428196a
adeclerk <adeclerk@gmail.com>
2013-04-20 19:17:58 -0400

    test

--- llticbackend/loginHandler.php
+++ llticbackend/loginHandler.php
@@ -6,6 +6,7 @@ require('class.Login.php');
 error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
   {
+  	print "IN";
 	$logmein = LoginUser();
 	//$login->loginUser($_POST['username'],$_POST['password']);
     
