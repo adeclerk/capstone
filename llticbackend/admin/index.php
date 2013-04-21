ab509ffffc596a2abd33930b47f7aea922239ea0
adeclerk <adeclerk@gmail.com>
2013-04-21 15:58:26 -0400

    tests

---
+++ llticbackend/admin/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once('../userin.php');
+
+if(userLoggedIn())
+{
+	if(getUserLoggedInLevel() != 2)
+	{
+		print "ERROR: You do not have permission to view this page";
+	}
+}
+?>
--- llticbackend/userin.php
+++ llticbackend/userin.php
@@ -5,7 +5,27 @@ function showUserLoggedIn()
 	print $_SESSION['user'];
 }
 
-require_once('Session.inc.php');
-$session = new Session();
-print $_SESSION['user'];
+function userLoggedIn()
+{
+	if($_SESSION['user'])
+		return true;
+	else
+		return false;
+}
+
+function getUserLoggedIn()
+{
+	if(userLoggedIn())
+		return $_SESSION['user'];
+	else
+		return '';
+}
+
+function getUserLoggedInLevel()
+{
+	if(userLoggedIn())
+		return $_SESSION['userLevel'];
+	else
+		return '';
+}
 ?>
