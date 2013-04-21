d2c798391e8f88746938452825b3aa0a8d3ff11a
adeclerk <adeclerk@gmail.com>
2013-04-20 22:47:30 -0400

    test

--- llticbackend/loginHandler.php
+++ llticbackend/loginHandler.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
 	if(loguserin($_POST['username'],$_POST['password']))
 	{
 		
-		if($_SESSION['userlevel'] == 2)
+		if($_SESSION['userlevel'] == "2")
 		{
 			showUserLoggedIn();
 		}
