9f334911263fc0b4bd621f1b52006b9b2d91f1df
adeclerk <adeclerk@gmail.com>
2013-04-21 18:51:26 -0400

    testing a change

--- llticbackend/logoutuser.php
+++ llticbackend/logoutuser.php
@@ -3,6 +3,6 @@ require ('UserSession.inc.php');
 $session = new UserSession();
 $session->deauthorize();
 
-header("Location: http://". $_SERVER['SERVER_NAME']);
+header("Location: http://". $_SERVER['SERVER_NAME'] ."");
 
 ?>
