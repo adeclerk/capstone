f498c4bd4043077b0b7a688c6ceb63ffbcb8525e
adeclerk <adeclerk@gmail.com>
2013-04-20 22:02:02 -0400

    login tests

--- llticbackend/loginHandler.php
+++ llticbackend/loginHandler.php
@@ -1,15 +1,40 @@
 <?php
 require ('Session.inc.php');
+require_once('LlticDbConnection.inc.php');
 include 'userin.php';
 $session = new Session();
 error_reporting(E_ALL);
 
-
+function loguserin($username,$pass)
+{
+	$db = new LlticDbConnection();
+	$userFound = $db->users->findUser($username);
+	if(!$userFound)
+	{
+		print "Login Error, user not found";
+		return false;
+	}
+	
+	if($pass != UserRecord::hashPass($pass))
+	{
+		print "Login error, password incorrect";
+		return false;
+	}
+	
+	$_SESSION['user'] = $username;
+	$_SESSION['pass'] = UserRecord::hashPass($pass);
+	return true;
+	
+}
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
   {
-	$_SESSION['user'] = $_POST['username'];
-	print $_SESSION['user'];
-	
-	showUserLoggedIn();
+	if(loguserin($_POST['username'],$_POST['password']))
+	{
+		showUserLoggedIn();
+	}
+	else
+	{
+		echo "Failed to login user.";
+	}
   }
 ?>
