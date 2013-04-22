cc605f87bee0023910c12900c77caff4d517c9da
adeclerk <adeclerk@gmail.com>
2013-04-21 22:41:31 -0400

    added messaging outline

---
+++ llticbackend/admin/sendMessageTab.php
@@ -0,0 +1,5 @@
+<form id='sendMessage' method='post' action='sendMessage.php'>
+	Recipient: <input type="text" id="recip">
+	Subject: <input type="text" id="subject">
+	Content: <input type="textarea" id="content">
+</form>
