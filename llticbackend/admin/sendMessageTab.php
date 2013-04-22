53d19701252d674ff094517b61dc4ae2f0d37bd0
adeclerk <adeclerk@gmail.com>
2013-04-21 22:44:46 -0400

    updated form

--- llticbackend/admin/sendMessageTab.php
+++ llticbackend/admin/sendMessageTab.php
@@ -1,5 +1,5 @@
 <form id='sendMessage' method='post' action='sendMessage.php'>
-	Recipient: <input type="text" id="recip">
-	Subject: <input type="text" id="subject">
-	Content: <input type="textarea" id="content">
+	Recipient: <input type="text" id="recip"><br/>
+	Subject: <input type="text" id="subject"><br/>
+	Content: <input type="textarea" id="content" style='width: 80%; height: 70%;'>
 </form>
