0d5c0c78bc8d99dd7b9374ab7ba52cab54391207
adeclerk <adeclerk@gmail.com>
2013-04-22 18:54:56 -0400

    updated views

--- llticbackend/views/view.login.php
+++ llticbackend/views/view.login.php
@@ -2,9 +2,9 @@
     <form action="controllers/con.login.php" method="post">
             <fieldset>
                     <legend>Login</legend>
-                    Username: <input type="text" name="username"> </br>
-                    Password: <input type="text" name="password"> </br>
-                    <input type="submit" name="formSubmit" value="Submit">
+                    Username: <input type="text" name="username"> <br/>
+                    Password: <input type="text" name="password"> <br/>
             </fieldset>
+            <input type="submit" name="formSubmit" value="Submit">
            </form>"
 </div>
