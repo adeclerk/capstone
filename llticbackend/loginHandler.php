2f41516b6edc1ccd76955f3de581e108905642ad
adeclerk <adeclerk@gmail.com>
2013-04-22 15:41:05 -0400

    updated structure

--- llticbackend/loginHandler.php
+++ llticbackend/loginHandler.php
@@ -3,49 +3,10 @@
 
 require_once('classes/class.UserSession.php');
 
-error_reporting(E_ALL);
-
-/*function loguserin($username,$pass)
-{
-	$db = new LlticDbConnection();
-	$userFound = $db->users->findUser($username);
-	$db->close();
-	if(!$userFound)
-	{
-		print "Login Error, user not found";
-		return false;
-	}
-	
-	if($userFound->getPassword() != UserRecord::hashPass($pass))
-	{
-		print "Login error, password incorrect";
-		return false;
-	}
-	//$_SESSION['user'] = $userFound->getUsername();
-	
-	$_SESSION['userLevel'] = $userFound->getUserLevel();
-	//$_SESSION['pass'] = UserRecord::hashPass($pass);
-
-	return true;
-	
-}
-*/
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
   {
-	/*if(loguserin($_POST['username'],$_POST['password']))
-	{
-		$db = new LlticDbConnection();
-		$user = $db->users->findUser($_SESSION['user']);
-		if($_SESSION['userLevel'] == 2)
-		{
-			//showUserLoggedIn();
-			print $_SESSION['user'];
-			print $session->getId();
-		}
-	
-	}
-	*/
-	$login = new UserSession($session,$_POST['username'],$_POST['password']);
+
+	$login = new UserSession(NULL,$_POST['username'],$_POST['password']);
   	if($login->autheticate())
   	{
   		switch($login->getUserRecord()->getUserLevel())
