e78291241196f222618e8ea18d1d3bf5f3f1ba23
adeclerk <adeclerk@gmail.com>
2013-04-21 22:49:22 -0400

    Added Input

--- llticbackend/admin/sendMessageTab.php
+++ llticbackend/admin/sendMessageTab.php
@@ -1,5 +1,6 @@
 <form id='sendMessage' method='post' action='sendMessage.php'>
 	Recipient: <input type="text" id="recip"><br/>
 	Subject: <input type="text" id="subject"><br/>
-	Content: <textarea style='width: 90%; height: 70%;' id='content'></textarea>
+	Content: <br/><textarea style='width: 90%; height: 70%;' id='content'></textarea>
+	<input type="submit" value="submit" id="submitForm">
 </form>
