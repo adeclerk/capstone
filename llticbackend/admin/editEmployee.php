21276cda71e1e294476b1d1dc8ee32431e340d7c
adeclerk <adeclerk@gmail.com>
2013-04-21 21:16:55 -0400

    fixed bug

--- llticbackend/admin/editEmployee.php
+++ llticbackend/admin/editEmployee.php
@@ -2,11 +2,11 @@
 require_once('../LlticDbConnection.inc.php');
 
 $db = new LlticDbConnection();
-$sql = "REPLACE INTO `employees` (`id`,`firstName`,`lastName`,`hireDate`,`salary`,`country`,`phone`,`userID`) VALUES('"
+$sql = "REPLACE INTO `employees` (`id`,`firstName`,`lastName`,`hireDate`,`salary`,`country`,`phone`) VALUES('"
 		. $_POST['id'] . "','" . $_POST['firstName'] . "','" 
 		. $_POST['lastName'] . "','" .$_POST['hireDate'] . "','"
 		. $_POST['salary'] . "','" . $_POST['country'] . "','"
-		. $_POST['phone'] . "','" . $_POST['userID'] . "')";
+		. $_POST['phone'] . "')";
 $result = $db->qry($sql);
 
 header ('Location: http://' . $_SERVER['SERVER_ADDR'] . '/admin');
