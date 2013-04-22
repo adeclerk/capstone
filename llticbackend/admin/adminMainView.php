90558b58f8275ffbfa9a52b681c780f2abdc15a4
adeclerk <adeclerk@gmail.com>
2013-04-21 21:47:02 -0400

    style

--- llticbackend/admin/adminMainView.php
+++ llticbackend/admin/adminMainView.php
@@ -14,7 +14,7 @@
 		<div id="title">
 		Admin Page 
 		</div>
-		<div id="righthead">
+		<div id="righthead" style="float: right;">
 		<?php print $this->user; ?>&nbsp;
 		(logout | preferences)
 		</div>
