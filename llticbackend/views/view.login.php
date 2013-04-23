6f75e32c124276061a9c24262a1a0d3c2c05c48f
adeclerk <adeclerk@gmail.com>
2013-04-22 21:35:07 -0400

    updated backend

--- llticbackend/views/view.login.php
+++ llticbackend/views/view.login.php
@@ -5,6 +5,11 @@
                     Username: <input type="text" name="username"> <br/>
                     Password: <input type="text" name="password"> <br/>
             </fieldset>
-            <input type="submit" name="formSubmit" value="Submit">
-           </form>"
+            <div id='forgot'>
+            <a href=''>forgot password?</a>
+            </div>
+            <div id='sub'>
+            	<input type="submit" name="formSubmit" value="Submit">
+            </div>
+           </form>
 </div>
