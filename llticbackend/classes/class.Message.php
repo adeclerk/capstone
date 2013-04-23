d2cf173f1371d035a4681bdef7c3f777bf0a0f35
adeclerk <adeclerk@gmail.com>
2013-04-22 23:34:00 -0400

    Update Message. Added MessageHandler.

--- llticbackend/classes/class.Message.php
+++ llticbackend/classes/class.Message.php
@@ -1,8 +1,8 @@
 <?php
-require_once ('class.LlticDbConnection.php');
+
 class Message
 {
-	private $db;
+
 	
 	public $id;
 	public $sId;
@@ -12,51 +12,24 @@ class Message
 	public $timestamp;
 	public $isRead;
 	
-	private function getNameFromId($userId)
+	public function __construct($row = NULL)
 	{
-		$sql = "SELECT employees.firstName, employees.lastName, users.username FROM employees JOIN users ON employees.userID = users.id WHERE userLevel='2'";
-		$ret = $this->db->qry($sql);
-		if(!$ret)
+		
+		if($row)
 		{
-			$sql = "SELECT employees.firstName, employees.lastName, users.username FROM employees JOIN users ON employees.userID = users.id WHERE userLevel='1'";
-			$ret = $this->db->qry($sql);
-			if(!$ret)
-			{
-				$sql = "SELECT employees.firstName, employees.lastName, users.username FROM employees JOIN users ON employees.userID = users.id WHERE userLevel='0'";
-				$ret = $this->db->qry($sql);
-				if(!$ret)
-				{
-					print "ERROR";
-					exit();
-				}
-				else
-				{
-					$row = $ret->fetch_assoc();
-					$string = $row['firstName'] ." " . $row['lastName'];
-				}
-			}
-			else
-			{
-				$row = $ret->fetch_assoc();
-				$string = $row['firstName'] ." " . $row['lastName'];
-			}
+			$this->id = $row['id'];
+			$this->sId = $row['sId'];
+			$this->rId = $row['rId'];
+			$this->sub = $row['subject'];
+			$this->content = $row['content'];
+			$this->timestamp = $row['timestamp'];
+			$this->isRead = $row['isRead'];
 		}
-		else 
-		{
-			$row = $ret->fetch_assoc();
-			$string = $row['firstName'] ." " . $row['lastName'];
-		}
-		return  $string;
-	}
-	
-	public function __construct()
-	{
-		$this->db = new LlticDbConnection();
 	}
 	
 	public function __destruct()
 	{
-		$this->db->__destruct();
+	
 	}
 	
 	public function set($row)
---
+++ llticbackend/classes/class.MessageHandler.php
@@ -0,0 +1,70 @@
+<?php
+require_once ('class.LlticDbConnection.php');
+require_once ('class.Message.php');
+
+class MessageHandler
+{
+	private $db;
+	
+	private function getNameFromId($userId)
+	{
+		$sql = "SELECT employees.firstName, employees.lastName, users.username FROM employees JOIN users ON employees.userID = users.id WHERE userLevel='2'";
+		$ret = $this->db->qry($sql);
+		if(!$ret)
+		{
+			$sql = "SELECT employees.firstName, employees.lastName, users.username FROM employees JOIN users ON employees.userID = users.id WHERE userLevel='1'";
+			$ret = $this->db->qry($sql);
+			if(!$ret)
+			{
+				$sql = "SELECT employees.firstName, employees.lastName, users.username FROM employees JOIN users ON employees.userID = users.id WHERE userLevel='0'";
+				$ret = $this->db->qry($sql);
+				if(!$ret)
+				{
+					print "ERROR";
+					exit();
+				}
+				else
+				{
+					$row = $ret->fetch_assoc();
+					$string = $row['firstName'] ." " . $row['lastName'];
+				}
+			}
+			else
+			{
+				$row = $ret->fetch_assoc();
+				$string = $row['firstName'] ." " . $row['lastName'];
+			}
+		}
+		else
+		{
+			$row = $ret->fetch_assoc();
+			$string = $row['firstName'] ." " . $row['lastName'];
+		}
+		return  $string;
+	}
+	
+	public function __construct()
+	{
+		$this->db = new LlticDbConnection();
+	}
+	
+	public function __destruct()
+	{
+		$this->db->__destruct();
+	}
+	
+	public function fetchMessages($userId)
+	{
+	
+	}
+	
+	public function fetchUnreadMessages($userId)
+	{
+	
+	}
+	
+	public function sendMessage($recipId,$subj,$content)
+	{
+	
+	}
+}
