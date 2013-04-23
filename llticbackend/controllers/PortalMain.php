eb62792312012e2f59e16a01307bff0641995e2a
adeclerk <adeclerk@gmail.com>
2013-04-23 19:00:35 -0400

    added template

--- llticbackend/controllers/PortalMain.php
+++ llticbackend/controllers/PortalMain.php
@@ -2,12 +2,16 @@
 require ('Controller.php');
 require_once ('classes/class.UserSession.php');
 require_once ('controllers/PortalLogin.php');
+require_once ('classes/class.Template.php');
 class PortalMain implements Controller
 {
 	private $session;
+	private $template;
 	public function __construct()
 	{
 		$this->session = new UserSession();
+		$this->template = new Template('views/view.portal.main.php');
+		$this->template->render();
 	}
 	
 	public function __destruct()
