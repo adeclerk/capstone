e0a7262233c83cb6d93c4ed0aea87508f9ae4f80
adeclerk <adeclerk@gmail.com>
2013-04-23 23:35:16 -0400

    test

--- llticbackend/controllers/PortalMain.php
+++ llticbackend/controllers/PortalMain.php
@@ -7,10 +7,12 @@ class PortalMain implements Controller
 {
 	private $session;
 	public $template;
+	public $controllers;
 	public function __construct()
 	{
 		$this->session = new UserSession();
 		$this->template = new Template('views/view.portal.main.php');
+		$this->controllers = array();
 	}
 	
 	public function __destruct()
@@ -26,7 +28,7 @@ class PortalMain implements Controller
 		}
 		else
 		{
-			$controller = new PortalLogin();
+			array_push($this->template->controllers,new PortalLogin());
 			$this->template->content = array();
 			array_push($this->template->content, $controller);
 			
