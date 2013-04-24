90063674f31728a2be2886c086169c57edc642d1
adeclerk <adeclerk@gmail.com>
2013-04-24 16:20:40 -0400

    made id escape string

--- llticbackend/classes/class.Session.php
+++ llticbackend/classes/class.Session.php
@@ -60,7 +60,7 @@ class Session
 
   private function read($sessionID)
   {
-    $sqlQuery = "SELECT * FROM `sessions` WHERE `id`='" . $sessionID ."'";
+    $sqlQuery = "SELECT * FROM `sessions` WHERE `id`='" . $this->dbcon->getConnection()->real_escape_string($sessionID) ."'";
 
     $result = $this->dbcon->qry($sqlQuery);
 	if($result->num_rows == 0)
