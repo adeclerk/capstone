282ad3c2ee8e32c8cf60dd396925885f69a3b12a
adeclerk <adeclerk@gmail.com>
2013-04-24 18:46:18 -0400

    debug

--- llticbackend/classes/class.Session.php
+++ llticbackend/classes/class.Session.php
@@ -60,7 +60,8 @@ class Session
 
   private function read($sessionID)
   {
-    $sqlQuery = "SELECT * FROM `sessions` WHERE `id`='" . $this->dbcon->getConnection()->real_escape_string($sessionID) ."'";
+    $sqlQuery = "SELECT * FROM `sessions` WHERE `id`='" . $sessionID ."'";
+    print "<br /> SEARCHING ON " . $sessionID . "<br/>";
 
     $result = $this->dbcon->qry($sqlQuery);
 	if($result->num_rows == 1)
