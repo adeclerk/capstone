bb12cad0616bf4fec52558b919d8856834fabb19
adeclerk <adeclerk@gmail.com>
2013-04-24 18:04:27 -0400

    test

--- llticbackend/controllers/Portal.AdminMain.php
+++ llticbackend/controllers/Portal.AdminMain.php
@@ -1,7 +1,7 @@
 <?php
-require_once ('../controllers/Controller.php');
-require_once ('../classes/class.Session.php');
-require_once ('../classes/class.Template.php');
+require_once ('Controller.php');
+require_once ('classes/class.Session.php');
+require_once ('classes/class.Template.php');
 
 class AdminMain implements Controller
 {
