69fd3ad5533b7d0a0a4bd59d9cb1576792828afa
adeclerk <adeclerk@gmail.com>
2013-04-24 19:48:34 -0400

    test

--- llticbackend/controllers/Portal.AdminMain.php
+++ llticbackend/controllers/Portal.AdminMain.php
@@ -2,6 +2,7 @@
 require_once ('Controller.php');
 require 'classes/class.Session.php';
 require 'classes/class.Template.php';
+require 'controllers/Portal.AdminEmployee.php';
 
 class AdminMain implements Controller
 {
@@ -31,7 +32,8 @@ class AdminMain implements Controller
 			$userWindow->windowtitle = "User";
 			$userWindow->windowcontent = new Template('views/view.portal.window.user.php', array(
 										'username' => $_SESSION['user']));
-			$this->view->content = $userWindow;
+			$contr = new AdminEmployee();
+			$this->view->content = $contr->invoke();
 			$this->view->render();
 		}
 		else
