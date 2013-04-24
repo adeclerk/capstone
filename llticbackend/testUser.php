2b2a32cf3e0813406ad1e2b4c0f3cfe357b7ca47
adeclerk <adeclerk@gmail.com>
2013-04-24 16:04:41 -0400

    tests

--- llticbackend/testUser.php
+++ llticbackend/testUser.php
@@ -1,8 +1,11 @@
 <?php
 require ('classes/class.User.php');
-
+require ('classes/class.Session.php');
+$session = new Session();
 $userTable = new User();
 
 print $userTable->read(2);
+$_SESSION['test'] = testVal;
+print $session->getId();
 
 ?>
