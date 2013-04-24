2a8398a155fad8d3eb17df9e611414fac228aebb
adeclerk <adeclerk@gmail.com>
2013-04-24 19:02:20 -0400

    updated view and styling

--- llticbackend/views/view.portal.window.user.php
+++ llticbackend/views/view.portal.window.user.php
@@ -1,4 +1,4 @@
-<div style='padding: 10px;'>
+<div>
 	<?php print $this->username; ?>
 	&nbsp;(&nbsp;<a href='logout.php'>logout</a>&nbsp;|&nbsp;<a href='preferences.php'>preferences</a>&nbsp;)
 </div>
