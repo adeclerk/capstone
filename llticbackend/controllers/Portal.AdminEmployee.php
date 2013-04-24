92e30c76eca560452dbabb095d44378fdb9c8456
adeclerk <adeclerk@gmail.com>
2013-04-24 19:51:46 -0400

    requires

---
+++ llticbackend/controllers/Portal.AdminEmployee.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'controllers/Controller.php';
+require_once 'classes/class.Template.php';
+require_once 'classes/class.Employee.php';
+
+class AdminEmployee implements Controller
+{
+	private $view;
+	private $employeeTable;
+	public function __construct()
+	{
+		$this->view = new Template('views/view.portal.admin.employees.php');
+		$this->employeeTable = new Employee();
+	}
+	
+	public function __destruct()
+	{
+		
+	}
+	
+	public function invoke()
+	{
+		$this->view->employees = $this->employeeTable->getAllEmployees();
+		return $this->view;
+	}
+}
+
+?>
