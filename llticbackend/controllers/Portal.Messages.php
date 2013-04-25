8a54af90ba12d24300586906cd2bb564195d8ba2
adeclerk <adeclerk@gmail.com>
2013-04-24 21:25:16 -0400

    session added

--- llticbackend/controllers/Portal.Messages.php
+++ llticbackend/controllers/Portal.Messages.php
@@ -2,13 +2,16 @@
 require_once 'Controller.php';
 require_once 'classes/class.Template.php';
 require_once 'classes/class.Message.php';
+require_once 'classes/class.Session.php';
 
 class PortalMessages implements Controller
 {
 	private $view;
 	private $messageTable;
+	private $session;
 	public function __construct()
 	{
+		$this->session = new Session();
 		$this->view = new Template('views/view.portal.window.php');
 		$this->view->windowname = "inbox";
 		$this->view->windowtitle = "Inbox";
@@ -18,7 +21,7 @@ class PortalMessages implements Controller
 	
 	public function __destruct()
 	{
-		
+		$this->session->__destruct();
 	}
 	
 	public function invoke()
