46d069df516c24d156c3fb31fa30a5766c4220c0
adeclerk <adeclerk@gmail.com>
2013-04-24 21:07:57 -0400

    sql

--- llticbackend/classes/class.Message.php
+++ llticbackend/classes/class.Message.php
@@ -76,8 +76,8 @@ class Message
 	
 	public function write($sid,$rid,$subj,$content)
 	{
-		$sql = "INSERT INTO `messages` (`sendID`,`recID`,`subject`,`content`) VALUES('" . $sid . "','" . $rid . "','" .$subj
-			. "','" . $content . "')";
+		$sql = "INSERT INTO `messages` (`sendID`,`recID`,`subject`,`content`) VALUES('" . $sid . "','" . $rid . "','" 
+		.$subj . "','" . $content . "')";
 		if($this->isOpen)
 		{
 			$this->dbcon->qry($sql);
@@ -85,7 +85,7 @@ class Message
 	}
 	public function getAllUnread($rid)
 	{
-		$sql = "SELECT * FROM `messages` WHERE `recID`='" .$rid . "' AND `isRead`=0";
+		$sql = "SELECT * FROM `messages` WHERE `recID`='" .$rid . "'";
 		if($this->isOpen)
 		{
 			$result = $this->dbcon->qry($sql);
