384fc47e22b267db319a59b80f9e2c0a7144a5fb
adeclerk <adeclerk@gmail.com>
2013-04-24 23:10:27 -0400

    updated js

--- llticbackend/views/view.portal.admin.employees.php
+++ llticbackend/views/view.portal.admin.employees.php
@@ -28,7 +28,7 @@ function addEmployeeView()
 	var window = document.getElementById("employees_content");
 	var form = document.getElementById("add_emp");
 	form.style.display = 'table-row';
-	window.innerHTML="Add Employee" + form;
+	window.innerHTML='<?php include 'views/view.portal.admin.employees.addForm.php'; ?>';
 }
 </script>
 <div style='text-align: right; height: 10px;'>
