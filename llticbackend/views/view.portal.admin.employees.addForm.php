6907dc169faf114cadb6f8e711cc1cc0e409fb38
adeclerk <adeclerk@gmail.com>
2013-04-24 23:35:41 -0400

    updated form

--- llticbackend/views/view.portal.admin.employees.addForm.php
+++ llticbackend/views/view.portal.admin.employees.addForm.php
@@ -1,28 +1,26 @@
-<table>
-	<tr>
-		<th>First Name</th>
-		<th>Last Name</th>
-		<th>Email</th>
-		<th>Username</th>
-		<th>User Level</th>
-		<th>Password</th>
-		<th>Country</th>
-		<th>Salary</th>
-		<th>Hire Date</th>
-		<th>Phone</th>
-		<th></th>
-	</tr>
-	<tr>
-		<td><input type='text' name='firstName' ></td>
-		<td><input type='text' name='lastName' ></td>
-		<td><input type='text' name='email' ></td>
-		<td><input type='text' name='username' ></td>
-		<td><input type='text' name='userLevel' ></td>
-		<td><input type='text' name='password' ></td>
-		<td><input type='text' name='country' ></td>
-		<td><input type='text' name='salary' ></td>
-		<td><input type='text' name='hireDate' > </td>
-		<td><input type='text' name='phone'></td>
-		<td><input type='button' name='add'></td>
-	</tr>
-</table>
+<form name="addemp" action="/portal/admin.php" method="post">
+	<fieldset>
+		<legend>User Details</legend>
+		Username: <input type="text" name="username"><br/>
+		Password: <input type="password" name="password1"><br/>
+		Repeat Password: <input type="password" name="password2"><br/>
+		Email: <input type="text" name="email"><br/>
+		User Level
+		<select name="userLevel">
+			<option value="2"> 2 (Admin) </option>
+			<option value="1"> 1 (Employee) </option>
+		</select>
+	</fieldset>
+	<fieldset>
+		<legend>Employee Details</legend>
+		<fieldset>
+			<legend>Name</legend>
+			First: <input type='text' name="firstName"><br/>
+			Last: <input type='text' name="lastName"><br/>
+		</fieldset>
+		Country: <input type='text' name="country"><br/>
+		Salary: <input type="text" name="salary"><br/>
+		Phone: <input type="text" name="phone"><br/>
+		Hire Date: <input type="text" name="hireDate">
+	</fieldset>
+</form>
