e3b40aca29bcf693e9e82941004e6d08c75ffe58
adeclerk <adeclerk@gmail.com>
2013-04-24 23:16:30 -0400

    added escapes

--- llticbackend/views/view.portal.admin.employees.addForm.php
+++ llticbackend/views/view.portal.admin.employees.addForm.php
@@ -1,28 +1,28 @@
-<table>
-	<tr>
-		<th>First Name</th>
-		<th>Last Name</th>
-		<th>Email</th>
-		<th>Username</th>
-		<th>User Level</th>
-		<th>Password</th>
-		<th>Country</th>
-		<th>Salary</th>
-		<th>Hire Date</th>
-		<th>Phone</th>
-		<th></th>
-	</tr>
-	<tr>
-		<td><input type='text' name='firstName' ></td>
-		<td><input type='text' name='lastName' ></td>
-		<td><input type='text' name='email' ></td>
-		<td><input type='text' name='username' ></td>
-		<td><input type='text' name='userLevel' ></td>
-		<td><input type='text' name='password' ></td>
-		<td><input type='text' name='country' ></td>
-		<td><input type='text' name='salary' ></td>
-		<td><input type='text' name='hireDate' > </td>
-		<td><input type='text' name='phone'></td>
-		<td><input type='button' name='add'></td>
-	</tr>
+<table> \
+	<tr> \
+		<th>First Name</th> \
+		<th>Last Name</th> \
+		<th>Email</th> \
+		<th>Username</th> \
+		<th>User Level</th> \
+		<th>Password</th> \
+		<th>Country</th> \
+		<th>Salary</th> \ 
+		<th>Hire Date</th> \
+		<th>Phone</th> \
+		<th></th> \
+	</tr> \
+	<tr> \
+		<td><input type='text' name='firstName' ></td> \
+		<td><input type='text' name='lastName' ></td> \
+		<td><input type='text' name='email' ></td> \
+		<td><input type='text' name='username' ></td> \
+		<td><input type='text' name='userLevel' ></td> \
+		<td><input type='text' name='password' ></td> \
+		<td><input type='text' name='country' ></td> \
+		<td><input type='text' name='salary' ></td> \
+		<td><input type='text' name='hireDate' > </td> \
+		<td><input type='text' name='phone'></td> \
+		<td><input type='button' name='add'></td>\
+	</tr> \
 </table>
