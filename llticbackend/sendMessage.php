c1aae97b2cd6add850e797b0c67b7c4713730523
adeclerk <adeclerk@gmail.com>
2013-04-25 13:26:51 -0400

    added debug

--- llticbackend/sendMessage.php
+++ llticbackend/sendMessage.php
@@ -9,6 +9,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')
 	$messageTable = new Message();
 	$userTable = new User();
 	$user = $userTable->getIdByUsername($_POST['recip']);
+	
+	print $_SESSION['uid'] . " <br/>"
+			. $user . "<br/>"
+			. $_POST['subject'] . "<br/>"
+			. $_POST['content'];
 	$messageTable->write($_SESSION['uid'],$user,$_POST['subject'],$_POST['content']);
 }
 
