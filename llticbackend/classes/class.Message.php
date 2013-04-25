435b435e2633910a64a3257eb6ce220b2b91490b
adeclerk <adeclerk@gmail.com>
2013-04-24 20:56:26 -0400

    typo fixed

--- llticbackend/classes/class.Message.php
+++ llticbackend/classes/class.Message.php
@@ -18,8 +18,8 @@ class Message
 		if($row)
 		{
 			$this->id = $row['id'];
-			$this->sId = $row['sId'];
-			$this->rId = $row['rId'];
+			$this->sId = $row['sendID'];
+			$this->rId = $row['recID'];
 			$this->sub = $row['subject'];
 			$this->content = $row['content'];
 			$this->timestamp = $row['timestamp'];
@@ -37,8 +37,8 @@ class Message
 	public function set($row)
 	{
 		$this->id = $row['id'];
-		$this->sId = $row['sId'];
-		$this->rId = $row['rId'];
+		$this->sId = $row['sendID'];
+		$this->rId = $row['recID'];
 		$this->sub = $row['subject'];
 		$this->content = $row['content'];
 		$this->timestamp = $row['timestamp'];
