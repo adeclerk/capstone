8f1bdc8304e44156f046cbec74d3421c254435d3
adeclerk <adeclerk@gmail.com>
2013-04-24 21:44:58 -0400

    test

--- llticbackend/testEmployee.php
+++ llticbackend/testEmployee.php
@@ -4,12 +4,12 @@ require 'classes/class.Employee.php';
 $emp = new Employee();
 
 $tEmp = $emp->getAllEmployees();
-
+$name = $emp->getNameByUserId(2);
+print $name['firstname'];
 foreach($tEmp as $employee)
 {
 	print $employee;
 }
-$name = $emp->getNameByUserId(2);
-print $name['firstname'];
+
 
 ?>
