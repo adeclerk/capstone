bf36a1b83f4211ed5c21807576c7465b225279c1
adeclerk <adeclerk@gmail.com>
2013-04-24 21:26:08 -0400

    test

--- llticbackend/controllers/Portal.Messages.php
+++ llticbackend/controllers/Portal.Messages.php
@@ -26,7 +26,7 @@ class PortalMessages implements Controller
 	
 	public function invoke()
 	{
-		$this->view->windowcontent->messages = array($this->messageTable->getAllUnread($_SESSION['uid']));
+		$this->view->windowcontent->messages = array($this->messageTable->getAllUnread(3));
 		return $this->view;
 	}
 }
