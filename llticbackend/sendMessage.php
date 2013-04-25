80df0dfe87ea3d8c296befceae8124f60ccec756
adeclerk <adeclerk@gmail.com>
2013-04-25 13:21:47 -0400

    update

--- llticbackend/sendMessage.php
+++ llticbackend/sendMessage.php
@@ -3,6 +3,7 @@ require_once 'classes/class.Message.php';
 require_once 'classes/class.Session.php';
 require_once 'classes/class.User.php';
 
+$session = new Session();
 if($_SERVER['REQUEST_METHOD'] == 'POST')
 {
 	$messageTable = new Message();
