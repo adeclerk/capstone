da3de9faeee2506f1ec3307d5d8240f298b43a94
adeclerk <adeclerk@gmail.com>
2013-04-24 21:22:01 -0400

    array update

--- llticbackend/controllers/Portal.Messages.php
+++ llticbackend/controllers/Portal.Messages.php
@@ -23,7 +23,7 @@ class PortalMessages implements Controller
 	
 	public function invoke()
 	{
-		$this->view->windowcontent->messages = $this->messageTable->read(0);
+		$this->view->windowcontent->messages = array($this->messageTable->read(0));
 		return $this->view;
 	}
 }
