48f7527fbd696b6544e27543b91a0adab524e241
adeclerk <adeclerk@gmail.com>
2013-04-25 22:02:47 -0400

    tests

--- llticbackend/views/view.portal.window.php
+++ llticbackend/views/view.portal.window.php
@@ -6,7 +6,7 @@ function minimize(windowID)
 }
 </script>
 <div id='<?php print $this->windowname; ?>' class='window'>
-	<div class='windowtitle' id='<?php print $this->windowname; ?>_title' onclick='minimize(<?php print $this->windowname; ?>)'>
+	<div class='windowtitle' id='<?php print $this->windowname; ?>_title' onclick='minimize("<?php print $this->windowname; ?>")'>
 	<?php print $this->windowtitle; ?>
 	</div>
 	<div class='windowcontent' id='<?php print $this->windowname . "_content"?>'>
