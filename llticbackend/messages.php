a4a07f7fdfd194812b400758e655409ceb56b3fd
adeclerk <adeclerk@gmail.com>
2013-04-25 21:55:43 -0400

    added session

--- llticbackend/messages.php
+++ llticbackend/messages.php
@@ -1,6 +1,7 @@
 <?php
 require_once ('classes/class.Message.php');
 require_once ('classes/class.Session.php');
+$session = new Session();
 $msgTable = new Message();
 
 $ret = $msgTable->getAllUnread($_SESSION['uid']);
