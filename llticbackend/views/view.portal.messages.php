68717e7990ecdc2a19f994d361b9505dd9609e5e
adeclerk <adeclerk@gmail.com>
2013-04-25 22:39:31 -0400

    test

--- llticbackend/views/view.portal.messages.php
+++ llticbackend/views/view.portal.messages.php
@@ -88,7 +88,7 @@ function hideMessage()
 		print "<div id='msg_" . $message->id . "' style='display: none;'>";
 		//print "<div style='text-align: left;' onclick='hideMessage(" . $message->id . "><- Back</div>";
 		print "<div style='text-align: left;'>" . $message->sender . " | " . $message->timestamp . "</div><hr/>";
-		print "<div style='text-align: left;'>Subject: " . $message->sub . " </div>";
+		print "<div style='text-align: left;'>Subject: " . $message->sub . " </div><hr/>";
 		print $message->content . "</div>";
 	}
 ?>
